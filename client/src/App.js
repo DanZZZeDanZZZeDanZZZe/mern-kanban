c7d71e9cda03f5198d19df013712fb246c0059f4
DanZZZeDanZZZeDanZZZe <jyjyjyj3@gmail.com>
2020-03-03 23:45:26 +0300

    add scrollbar styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.scss';
+import './additionalStyles.css';
 import { Navbar } from './components/Navbar/Navbar';
 import { Footer } from './components/Footer/Footer';
 import { useRoutes } from './routes';
