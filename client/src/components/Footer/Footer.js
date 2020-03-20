6db60229232a2f4ae6786909790823b34358809b
DanZZZeDanZZZeDanZZZe <jyjyjyj3@gmail.com>
2020-03-20 23:57:20 +0300

    added statistics in the footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React,{ useContext, useState, useEffect } from 'react';
 import './Footer.css';
+import { taskBarContext } from '../../context/TaskBar/taskBarContext';
+
 
 export const Footer = () => {
+    const initState = (tasksState) => {
+        return {
+            activedTasks: tasksState[0].issues.length,
+            finishedTasks: tasksState[tasksState.length-1].issues.length, 
+        }
+    }
 
+    const {tasksState} = useContext(taskBarContext)
+    const [footerState, setFooterState] = useState(initState(tasksState))
+    
+    useEffect(() => {
+        setFooterState(initState(tasksState))
+    }, [tasksState]);
+   
     return (
         <div className="Footer">
-
+            <p>{`Actived tasks: ${footerState.activedTasks}`}</p>
+            <p>{`Finished tasks: ${footerState.finishedTasks}`}</p>
         </div>
     )
 }
